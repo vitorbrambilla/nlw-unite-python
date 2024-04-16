e62c85fb107cc60a886e76f746577ddb3b5de695
Vitor <vitor.reis@crmservices.com.br>
2024-04-16 10:57:10 -0300

    feat: implementing flask with server

---
+++ src/main/routes/event_routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify
+
+event_route_bp = Blueprint('event_route', __name__)
+
+
+@event_route_bp.route('/events', methods=['POST'])
+def create_event():
+    return jsonify({'message': 'Event created'}), 201
---
+++ src/main/server/server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+from src.main.routes.event_routes import event_route_bp
+
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(event_route_bp)
