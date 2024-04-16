8a5c6206c201e2478b680e616a2e239ad352458b
Vitor <vitor.reis@crmservices.com.br>
2024-04-16 08:46:02 -0300

    refactor: try in events repository

--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -1,31 +1,43 @@
 from typing import Dict
 from src.models.settings.connection import db_connection_handler
 from src.models.entities.events import Events
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class EventsRepository:
     def insert_event(self, eventsInfo: Dict) -> Dict:
         with db_connection_handler as database:
-            event = Events(
-                id=eventsInfo.get("uuid"),
-                title=eventsInfo.get("title"),
-                details=eventsInfo.get("details"),
-                slug=eventsInfo.get("slug"),
-                maximum_attendees=eventsInfo.get("maximum_attendees"),
-            )
+            try:
+                event = Events(
+                    id=eventsInfo.get("uuid"),
+                    title=eventsInfo.get("title"),
+                    details=eventsInfo.get("details"),
+                    slug=eventsInfo.get("slug"),
+                    maximum_attendees=eventsInfo.get("maximum_attendees"),
+                )
 
-            database.session.add(event)
-            database.session.commit()
+                database.session.add(event)
+                database.session.commit()
 
-            return eventsInfo
+                return eventsInfo
+            except IntegrityError:
+                raise Exception("Event already exists")
+
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
 
     def get_event_by_id(self, event_id: str) -> Events:
-        with db_connection_handler as database:
-            event = (
-                database.session
-                .query(Events)
-                .filter(Events.id == event_id)
-                .one()
-            )
+        try:
+            with db_connection_handler as database:
+                event = (
+                    database.session
+                    .query(Events)
+                    .filter(Events.id == event_id)
+                    .one()
+                )
 
-            return event
+                return event
+        except NoResultFound:
+            return None
